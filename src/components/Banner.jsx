20fc13703691c35c0bf3d0a36ed51de1db18b6b9
vishalicious213 <vishalicious213@gmail.com>
2024-02-26 14:33:07 -0800

    Banner: checking type prop

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 
 const Banner = ({
-    type=false, 
-    textColor="#1F2937", 
-    backgroundColor="#F3F4F6", 
-    typeText="Badge",
-    bodyText="Message text here"
+    type="neutral", 
+    typeText="Update available",
+    bodyText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
 }) => {
+    let bannerStyle = {}
+
+    if (type === "success") {
+        console.log("Success")
+    } else if (type === "warning") {
+        console.log("Warning")
+    } else if (type === "error") {
+        console.log("Error")
+    } else {
+        console.log("Neutral")
+    }
+
     return (
         <div
             style={{
-                    color: textColor, 
-                    backgroundColor: backgroundColor,
+                    // color: textColor, 
+                    // backgroundColor: backgroundColor,
                     borderRadius: ".25rem",
                     padding: ".25rem .75rem",
                     margin: "0 .25rem",
@@ -26,3 +36,9 @@ const Banner = ({
 }
 
 export default Banner
+
+// type=false, 
+// textColor="#1F2937", 
+// backgroundColor="#F3F4F6", 
+// typeText="Badge",
+// bodyText="Message text here"
