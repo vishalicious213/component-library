fe5d463590a67b67ea90442fd7bbdce30d38658f
vishalicious213 <vishalicious213@gmail.com>
2024-02-26 13:17:57 -0800

    App: rendering square buttons with text and background colors

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ import Button from './components/Button'
 const App = () => {
     return (
         <div id="app">
-            <Button />
+            <Button textColor="#1F2937" backgroundColor="#F3F4F6" />
+            <Button textColor="#991B1B" backgroundColor="#FEE2E2" />
+            <Button textColor="#92400E" backgroundColor="#FEF3C7" />
+            <Button textColor="#065F46" backgroundColor="#D1FAE5" />
+            <Button textColor="#1E40AF" backgroundColor="#DBEAFE" />
+            <Button textColor="#3730A3" backgroundColor="#E0E7FF" />
+            <Button textColor="#5B21B6" backgroundColor="#EDE9FE" />
+            <Button textColor="#9D174D" backgroundColor="#FCE7F3" />
         </div>
     )
 }
