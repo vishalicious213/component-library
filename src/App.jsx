5803b53280d78c9d9569c9056ecafadd55860778
vishalicious213 <vishalicious213@gmail.com>
2024-02-26 13:22:14 -0800

    App: rendering pill buttons with text and background colors

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,27 @@ import Button from './components/Button'
 const App = () => {
     return (
         <div id="app">
-            <Button textColor="#1F2937" backgroundColor="#F3F4F6" />
-            <Button textColor="#991B1B" backgroundColor="#FEE2E2" />
-            <Button textColor="#92400E" backgroundColor="#FEF3C7" />
-            <Button textColor="#065F46" backgroundColor="#D1FAE5" />
-            <Button textColor="#1E40AF" backgroundColor="#DBEAFE" />
-            <Button textColor="#3730A3" backgroundColor="#E0E7FF" />
-            <Button textColor="#5B21B6" backgroundColor="#EDE9FE" />
-            <Button textColor="#9D174D" backgroundColor="#FCE7F3" />
+            <div className="container">
+                <Button textColor="#1F2937" backgroundColor="#F3F4F6" />
+                <Button textColor="#991B1B" backgroundColor="#FEE2E2" />
+                <Button textColor="#92400E" backgroundColor="#FEF3C7" />
+                <Button textColor="#065F46" backgroundColor="#D1FAE5" />
+                <Button textColor="#1E40AF" backgroundColor="#DBEAFE" />
+                <Button textColor="#3730A3" backgroundColor="#E0E7FF" />
+                <Button textColor="#5B21B6" backgroundColor="#EDE9FE" />
+                <Button textColor="#9D174D" backgroundColor="#FCE7F3" />
+            </div>
+
+            <div className="container">
+                <Button textColor="#1F2937" backgroundColor="#F3F4F6" pill />
+                <Button textColor="#991B1B" backgroundColor="#FEE2E2" pill />
+                <Button textColor="#92400E" backgroundColor="#FEF3C7" pill />
+                <Button textColor="#065F46" backgroundColor="#D1FAE5" pill />
+                <Button textColor="#1E40AF" backgroundColor="#DBEAFE" pill />
+                <Button textColor="#3730A3" backgroundColor="#E0E7FF" pill />
+                <Button textColor="#5B21B6" backgroundColor="#EDE9FE" pill />
+                <Button textColor="#9D174D" backgroundColor="#FCE7F3" pill />
+            </div>
         </div>
     )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,6 +8,7 @@ const Button = ({pill=false, textColor="#1F2937", backgroundColor="#F3F4F6", tex
                     color: textColor, 
                     backgroundColor: backgroundColor,
                     padding: ".25rem .75rem",
+                    margin: ".5rem",
                     border: "none"
             }}
         >
