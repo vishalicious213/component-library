467e18285c7c38213c6b7bf8951e46693e96d7c6
vishalicious213 <vishalicious213@gmail.com>
2024-02-26 12:57:00 -0800

    imported Inter font. set up .inter css class. applied class to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Button = ({style="square", textColor="#1F2937", backgroundColor="#F3F4F6", text="Badge"}) => {
     return (
-        <div>Button</div>
+        <button 
+            className="inter"
+            style={{
+                    color: textColor, 
+                    backgroundColor: backgroundColor,
+                    border: "none"
+            }}
+        >
+            {text}
+        </button>
     )
 }
 
