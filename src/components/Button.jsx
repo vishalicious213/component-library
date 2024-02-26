74b151a3ecacc04375dc7df022aea1df98794885
vishalicious213 <vishalicious213@gmail.com>
2024-02-26 13:51:58 -0800

    Button: removed className and used ternary in style to set border-radius. CSS: removed .pill

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
-const Button = ({pill=false, textColor="#1F2937", backgroundColor="#F3F4F6", text="Badge"}) => {
+const Button = ({
+    pill=false, 
+    textColor="#1F2937", 
+    backgroundColor="#F3F4F6", 
+    text="Badge"
+}) => {
     return (
         <button 
-            className={pill ? "inter pill" : "inter"}
             style={{
                     color: textColor, 
                     backgroundColor: backgroundColor,
+                    borderRadius: pill ? "1rem" : ".25rem",
                     padding: ".25rem .75rem",
                     margin: ".5rem",
                     border: "none"
