bd403ce78536c8f447f78e7675bb73958d80fd9b
vishalicious213 <vishalicious213@gmail.com>
2024-02-26 14:54:51 -0800

    Banner: updated where style info is held. added neutral options

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,26 +2,39 @@ import React from 'react'
 
 const Banner = ({
     type="neutral", 
-    typeText="Update available",
-    bodyText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
 }) => {
     let bannerStyle = {}
 
     if (type === "success") {
         console.log("Success")
+        bannerStyle = {
+            titleColor: "#065F46",
+            bodyColor: "#047857",
+            backgroundColor: "#ECFDF5",
+            typeText: "Congratulations!",
+            bodyText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
+        }
     } else if (type === "warning") {
         console.log("Warning")
     } else if (type === "error") {
         console.log("Error")
     } else {
         console.log("Neutral")
+        bannerStyle = {
+            titleColor: "#1E40AF",
+            bodyColor: "#1C51B9",
+            backgroundColor: "#EFF6FF",
+            typeText: "Update available",
+            bodyText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
+                }
+
     }
 
     return (
         <div
+            className="inter"
             style={{
-                    // color: textColor, 
-                    // backgroundColor: backgroundColor,
+                    backgroundColor: bannerStyle.backgroundColor,
                     borderRadius: ".25rem",
                     padding: ".25rem .75rem",
                     margin: "0 .25rem",
@@ -29,8 +42,8 @@ const Banner = ({
                     border: "none"
             }}
         >
-            <p>{typeText}</p>
-            <p>{type ? bodyText : null}</p>
+            <p style={{fontWeight: 600, color: bannerStyle.titleColor}}>{bannerStyle.typeText}</p>
+            <p style={{fontWeight: 400, color: bannerStyle.bodyColor}}>{type ? bannerStyle.bodyText : null}</p>
         </div>
     )
 }
