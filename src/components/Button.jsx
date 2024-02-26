1d83a419047b7db5c152ea5e7efc6cb1eb7d4902
vishalicious213 <vishalicious213@gmail.com>
2024-02-26 12:41:01 -0800

    Button: importing props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({style="square", textColor="#1F2937", backgroundColor="#F3F4F6", text="Badge"}) => {
     return (
         <div>Button</div>
     )
