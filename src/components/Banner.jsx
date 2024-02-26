2eb8dd5a62d361aa20415f0c2aeefecfadc25164
vishalicious213 <vishalicious213@gmail.com>
2024-02-26 14:18:02 -0800

    drafted /components/Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
-const Banner = () => {
+const Banner = ({
+    type=false, 
+    textColor="#1F2937", 
+    backgroundColor="#F3F4F6", 
+    typeText="Badge",
+    bodyText="Message text here"
+}) => {
     return (
-        <div>Banner</div>
+        <div
+            style={{
+                    color: textColor, 
+                    backgroundColor: backgroundColor,
+                    borderRadius: ".25rem",
+                    padding: ".25rem .75rem",
+                    margin: "0 .25rem",
+                    width: "50%",
+                    border: "none"
+            }}
+        >
+            <p>{typeText}</p>
+            <p>{type ? bodyText : null}</p>
+        </div>
     )
 }
 
