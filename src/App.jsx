2aa2c84fe51cd405c54d3bec2d4601da1e920c6d
vishalicious213 <vishalicious213@gmail.com>
2024-02-26 12:37:19 -0800

    updated App to render Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Button from './components/Button'
 
 const App = () => {
     return (
-        <div>App</div>
+        <div id="app">
+            <Button />
+        </div>
     )
 }
 
