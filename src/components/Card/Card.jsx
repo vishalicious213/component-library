0c6f9180dc25a9409d78c606fe3d4513ce0286ae
vishalicious213 <vishalicious213@gmail.com>
2024-03-11 10:54:17 -0700

    /components/Card/Card: rendering brand-icon.svg

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,6 +5,7 @@ const Card = () => {
         <div>
             <h2 className="marker">Cards</h2>
             <div className='card inter'>
+                <img src="/brand-icon.svg" alt="" />
                 <h3>Easy Deployment</h3>
                 <p>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
             </div>
