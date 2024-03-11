922d7bfcfe04decfd961df9b856f6dacb23bc094
vishalicious213 <vishalicious213@gmail.com>
2024-03-11 15:59:05 -0700

    Testimonial: set up browser width state and added ternary to change layout of name/title

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Testimonial = ({
     pic,
@@ -7,13 +8,24 @@ const Testimonial = ({
     name,
     title
 }) => {
+    const [width, setWidth] = useState(window.innerWidth)
+
+    function checkWidth() {
+        setWidth(window.innerWidth)
+    }
+    
+    window.addEventListener("resize", checkWidth)
+
     return (
         <div className="test-wrapper">
             <div className="testimonial inter">
                 <img src={pic} alt="" />
                 <img className="test-logo" src={logo} alt="" />
                 <p className="test-text">{text}</p>
-                <p>{name} / <span class="test-title">{title}</span></p>
+                {width > 500 
+                    ? <p>{name} / <span className="test-title">{title}</span></p>
+                    : <><p>{name}</p><p className="test-title">{title}</p></>
+                }
             </div>
         </div>
     )
