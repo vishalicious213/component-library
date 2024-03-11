32fa3a73245e88bbed83143c3b4e5d48ddea744b
vishalicious213 <vishalicious213@gmail.com>
2024-03-11 11:22:27 -0700

    App: rendering Badge, Banner and Card

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     return (
         <div id="app">
             <BadgeExample />
-            {/* <BannerExample /> */}
+            <BannerExample />
             <CardExample />
         </div>
     )
