3643e58f0968888d4eb7e644c1150b23078d76df
vishalicious213 <vishalicious213@gmail.com>
2024-03-11 13:29:57 -0700

    dradted /components/Testimonials/Testimonial-Example.jsx

---
+++ src/components/Testimonials/Testimonial-Example.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Testimonial from './Testimonial'
+
+const TestimonialExample = () => {
+    return (
+        <div className="container">
+            <Testimonial />
+        </div>
+    )
+}
+
+export default TestimonialExample
