9149d871e4b0f1e496616108ba40040147568f88
vishalicious213 <vishalicious213@gmail.com>
2024-03-11 13:24:47 -0700

    /components/Card/Card-Example: added container to render cards and made it flex

--- src/components/Card/Card-Example.jsx
+++ src/components/Card/Card-Example.jsx
@@ -5,12 +5,14 @@ const CardExample = () => {
     return (
         <div className="container">
             <h2 className="marker">Cards</h2>
-            <Card />
-            <Card
-                icon="/error.svg"
-                title="Uneasy Deployment"
-                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-            />
+            <div className="cards">
+                <Card />
+                <Card
+                    icon="/error.svg"
+                    title="Uneasy Deployment"
+                    desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                />
+            </div>
         </div>
     )
 }
