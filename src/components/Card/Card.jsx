18b218a35c46ffd38caaac0c1b1acae8d478a762
vishalicious213 <vishalicious213@gmail.com>
2024-03-11 11:13:35 -0700

    /components/Card/Card: updated component to use props

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({
+    icon="/brand-icon.svg", 
+    title="Easy Deployment", 
+    desc="Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis."
+}) => {
     return (
         <div>
             <h2 className="marker">Cards</h2>
             <div className='card inter'>
-                <img src="/brand-icon.svg" alt="" />
-                <h3>Easy Deployment</h3>
-                <p>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
+                <img src={icon} alt="" />
+                <h3>{title}</h3>
+                <p>{desc}</p>
             </div>
         </div>
     )
