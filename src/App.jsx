78014b09fda918616a17629ac62bc8d22227e78e
vishalicious213 <vishalicious213@gmail.com>
2024-03-11 12:43:21 -0700

    updated App with buttons and state to render components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
 import BadgeExample from './components/Badge/Badge-Example'
 import BannerExample from './components/Banner/Banner-Example'
 import CardExample from './components/Card/Card-Example'
 
 const App = () => {
+    const [clicked, setClicked] = useState()
+
+    function handleClick(option) {
+        setClicked(option)
+    }
+
     return (
         <div id="app">
-            <BadgeExample />
-            <BannerExample />
-            <CardExample />
+            <button onClick={() => handleClick("badge")}>Badge</button>
+            <button onClick={() => handleClick("banner")}>Banner</button>
+            <button onClick={() => handleClick("card")}>Card</button>
+
+            <div>
+                {
+                    clicked === "badge" ? <BadgeExample /> 
+                    : clicked === "banner" ? <BannerExample />
+                    : clicked === "card" ? <CardExample />
+                    : null
+                }
+            </div>
         </div>
     )
 }
