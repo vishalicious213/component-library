6cca3a800500c696d4cc78476f8f0affaf327269
vishalicious213 <vishalicious213@gmail.com>
2024-03-12 17:52:24 -0700

    Testimonial: removed code for profile pic. drafted TestimonialWithPic

--- src/components/Testimonials/Testimonial-Example.jsx
+++ src/components/Testimonials/Testimonial-Example.jsx
@@ -6,7 +6,7 @@ const TestimonialExample = () => {
         <div className="container">
             <h2 className="marker">Testimonials</h2>
             <Testimonial
-                    pic="/avatar.png"
+                    // pic="/avatar.png"
                     logo="/logo.svg"
                     text="“Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.”"
                     name="May Andersons"
--- src/components/Testimonials/TestimonialWithPic.jsx
+++ src/components/Testimonials/TestimonialWithPic.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Testimonial = ({
+    pic,
     logo,
     text,
     name,
@@ -9,9 +10,18 @@ const Testimonial = ({
     return (
         <div className="test-wrapper">
             <div className="testimonial inter">
-                <div className="test-logo-container">
-                    <img className="test-logo" src={logo} alt="" />
-                </div>
+                {/* {pic ? 
+                    <div className="test-img-container">
+                        <img className="test-img" src={pic} alt="" />
+                    </div>
+                    : null
+                } */}
+                {!pic ? 
+                    <div className="test-logo-container">
+                        <img className="test-logo" src={logo} alt="" />
+                    </div>
+                    : null
+                }
                 <p className="test-text">{text}</p>
                 <p className="title-lg">{name} / <span className="test-title">{title}</span></p>
                 <div className="title-sm"><p>{name}</p><p className="test-title">{title}</p></div>
