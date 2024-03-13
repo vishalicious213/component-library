6165f13529fe2a550db375f75e59bb386f6939e6
vishalicious213 <vishalicious213@gmail.com>
2024-03-13 09:31:41 -0700

    added /public/quote.svg. rendered in TestimonialWithPic

--- src/components/Testimonials/TestimonialWithPic.jsx
+++ src/components/Testimonials/TestimonialWithPic.jsx
@@ -7,11 +7,12 @@ const TestimonialWithPic = ({
     title
 }) => {
     return (
-        <div className="test-wrapper">
-            <div className="testimonial inter">
+        <div className="test-wrapper blue">
+            <div className="testimonial inter blue">
                 <div className="test-img-container">
                     <img className="test-img" src={pic} alt="" />
                 </div>
+                <img src="/quote.svg" alt="" />
                 <p className="test-text">{text}</p>
                 <p className="title-lg">{name} / <span className="test-title">{title}</span></p>
                 <div className="title-sm"><p>{name}</p><p className="test-title">{title}</p></div>
